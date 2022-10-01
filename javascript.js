292e1dcc573e379689ec407e839e9da6e85cc939
juswajek <111828480+juswajek@users.noreply.github.com>
2022-10-01 20:00:21 +0800

    navigation

--- javascript.js
+++ javascript.js
@@ -12,9 +12,10 @@
 
         function Menu(e) {
             let list = document.querySelector('ul');
-            e.name === 'menu' ? 
-            (e.name = "close", list.classList.add('top-[80px]'), list.classList.add('opacity-100'))
-             : (e.name = "menu", list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'))
+            let mbtn = document.querySelector('#menu')
+
+            mbtn.classList.toggle('fa-xmark');
+            mbtn.classList.toggle('fa-bars');
 
              list.classList.toggle('hidden');
              list.classList.toggle('block')
